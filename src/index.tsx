875046981eba4c3aca35ed574b255e988912891b
soyma20 <slaviksoyma2014@gmail.com>
2022-11-10 20:32:38 +0200

    feat: connect store and routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
+import {setupStore} from "./redux";
 import {App} from './App';
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const store = setupStore();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
