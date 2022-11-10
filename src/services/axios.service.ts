73d8d8e79bdd98b6458d50a3ffe765a4efab0090
soyma20 <slaviksoyma2014@gmail.com>
2022-11-10 18:37:25 +0200

    feat: add axios service

---
+++ src/services/axios.service.ts
@@ -0,0 +1,10 @@
+import axios, {AxiosResponse} from "axios";
+import {baseURl} from "../constants";
+
+let axiosService = axios.create({baseURL:baseURl});
+
+export type Res<T> = Promise<AxiosResponse<T>>;
+
+export {
+    axiosService
+}
