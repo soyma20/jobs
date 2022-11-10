9db987852be1d103f7cdd19b37800c6985f12068
soyma20 <slaviksoyma2014@gmail.com>
2022-11-10 18:38:25 +0200

    feat: add job component with base structure

---
+++ src/components/Job/Job.tsx
@@ -0,0 +1,16 @@
+import {FC} from "react";
+import {IJob} from "../../interfaces";
+
+interface IProps{
+    job: IJob
+}
+
+const Job: FC<IProps> = ({job}) => {
+    return (
+        <div>
+            ${job.name}
+        </div>
+    );
+};
+
+export {Job};
