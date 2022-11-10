5906b620e819c02bc33aedbec35f171693713f82
soyma20 <slaviksoyma2014@gmail.com>
2022-11-10 20:27:03 +0200

    feat: add routing for job list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import {FC} from "react";
-import {Jobs} from "./components";
+import {Navigate, Route, Routes} from "react-router-dom";
+
+import {MainLayout} from "./layouts";
+import {JobListPage} from "./pages";
 
 const App: FC = () => {
     return (
-        <Jobs/>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}/>
+            <Route index element={<Navigate to={'/jobs'}/>}/>
+                <Route path={'/jobs'} element={<JobListPage/>}/>
+        </Routes>
     );
 };
 
