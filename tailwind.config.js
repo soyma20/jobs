208298f0ff0a2dfff5aec221c6567f0071356cc5
soyma20 <slaviksoyma2014@gmail.com>
2022-11-15 19:25:33 +0200

    feat: custom font and box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      base: ['16px', '25p'],
+      xl: ['20px', '25px'],
+    },
+    extend: {
+      boxShadow: {
+        'md': ' 2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12)',
+      },
+    }
   },
   plugins: [],
 }
