8c9ad528663bf327276f82109cc74b5a16dd7331
soyma20 <slaviksoyma2014@gmail.com>
2022-11-18 18:07:48 +0200

    feat: add chip component

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 const Chip: FC<IProps> = ({chip,textColor,bgColor,borderColor}) => {
 
-    let className=`sm:w-fit rounded-lg font-bold  text-center pt-[3px] mr-[8px] border text-base w-[222px] h-[39px] bg-[${bgColor}] text-[${textColor}] border-2`
+    let className=`sm:w-fit rounded-lg font-bold  text-center pt-[3px] mr-[8px] border text-base w-[222px] h-[39px] bg-[${bgColor}] text-[${textColor}] border-2 border-[${borderColor}]`
 
     return (
         <div
