2988f973221af325f6a2aaece160e07d72e87a22
soyma20 <slaviksoyma2014@gmail.com>
2022-11-18 18:08:19 +0200

    feat: add contacts component

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,27 @@
+import {FC} from "react";
+
+import Map from "../../assets/img/Map.svg";
+import {IJob} from "../../interfaces";
+
+interface IProps {
+    job: IJob
+}
+
+const Contacts: FC<IProps> = ({job}) => {
+    return (
+        <div className="ml-[133px] sm:ml-[0] h-[436px] w-[402px]">
+            <div className="h-[218px] bg-[#2A3047] pt-[31px] rounded-t-lg">
+                <div className="ml-[62px] ">
+                    <h5 className="text-white text-xl font-bold mb-[8px]">Department name.<br/>
+                        {job.name}
+                    </h5>
+                    <p className="text-white text-lg mb-[7px]">{job.address}</p>
+                    <p className="text-white text-lg">{job.phone}, <br/> {job.email}</p>
+                </div>
+            </div>
+            <img className="w-[402px] rounded-b-lg" src={Map} alt="Map"/>
+        </div>
+    );
+};
+
+export {Contacts};
