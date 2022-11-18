04eee5ff41e7ad73cead4b9fa67032d9a43cd3f6
soyma20 <slaviksoyma2014@gmail.com>
2022-11-18 18:04:22 +0200

    feat: add job details routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import {FC} from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
 
 import {MainLayout} from "./layouts";
-import {JobListPage} from "./pages";
+import {DetailedJobPage, JobListPage} from "./pages";
 
 const App: FC = () => {
     return (
         <Routes>
             <Route path={'/'} element={<MainLayout/>}/>
             <Route index element={<Navigate to={'/jobs'}/>}/>
-                <Route path={'/jobs'} element={<JobListPage/>}/>
+            <Route path={'jobs'} element={<JobListPage/>}/>
+            <Route path={'jobs/:id'} element={<DetailedJobPage/>}/>
         </Routes>
     );
 };
